18ef5c1f954197c604c05bc8f2882fd72ddd0000
JasonRen23 <zhicheng_ren@163.com>
2018-03-18 17:10:26 +0800

    add controller code

---
+++ src/main/java/cn/codingcheng/web/SeckillController.java
@@ -0,0 +1,103 @@
+package cn.codingcheng.web;
+
+import cn.codingcheng.dto.Exposer;
+import cn.codingcheng.dto.SeckillExecution;
+import cn.codingcheng.dto.SeckillResult;
+import cn.codingcheng.entity.Seckill;
+import cn.codingcheng.enums.SeckillStateEnum;
+import cn.codingcheng.exception.RepeatKillException;
+import cn.codingcheng.exception.SeckillCloseException;
+import cn.codingcheng.service.SeckillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.test.annotation.Repeat;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@Component //url:模块/资源/{}/细分
+@RequestMapping("/seckill")
+public class SeckillController {
+    @Autowired
+    private SeckillService seckillService;
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list(Model model){
+        //list.jsp+mode=ModelAndView
+        //获取列表页
+        List<Seckill> list = seckillService.getSeckillList();
+        model.addAttribute("list", list);
+        return "list";
+    }
+
+    @RequestMapping(value = "/{seckillId}/detail", method = RequestMethod.GET)
+    public String detail(@PathVariable("seckillId") Long seckillId, Model model){
+        if(seckillId == null){
+            return "redirect:/seckill/list";
+        }
+        Seckill seckill = seckillService.getById(seckillId);
+        if(seckill == null){
+            return "forward:/seckill/list";
+        }
+
+        model.addAttribute("seckill", seckill);
+
+        return "detail";
+    }
+
+    //ajax, json暴露秒杀接口的方法
+    @RequestMapping(value = "/{seckillId}/exposer",
+            method = RequestMethod.POST,
+            produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
+    public SeckillResult<Exposer> exposer(Long seckillId){
+        SeckillResult<Exposer> result;
+        try{
+            Exposer exposer = seckillService.exportSeckillUrl(seckillId);
+            result = new SeckillResult<Exposer>(true, exposer);
+        }catch (Exception e){
+            e.printStackTrace();
+            result = new SeckillResult<Exposer>(false, e.getMessage());
+    }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/{seckillId}/{md5}/execution",
+                    method = RequestMethod.POST,
+                    produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
+    public SeckillResult<SeckillExecution> execute(@PathVariable("seckillId") Long seckillId,
+                                                   @PathVariable("md5") String md5,
+                                                   @CookieValue(value = "killPhone", required = false) Long phone){
+        if(phone == null){
+            return new SeckillResult<SeckillExecution>(false, "未注册");
+        }
+        SeckillResult<SeckillExecution> result;
+
+        try{
+            SeckillExecution execution = seckillService.executeSeckill(seckillId, phone, md5);
+            return new SeckillResult<SeckillExecution>(true, execution);
+        }catch (RepeatKillException e1){
+            SeckillExecution execution = new SeckillExecution(seckillId, SeckillStateEnum.REPEAT_KILL);
+            return new SeckillResult<SeckillExecution>(false, execution);
+        }catch (SeckillCloseException e2){
+            SeckillExecution execution = new SeckillExecution(seckillId, SeckillStateEnum.END);
+            return new SeckillResult<SeckillExecution>(false, execution);
+        }catch (Exception e){
+            SeckillExecution execution = new SeckillExecution(seckillId, SeckillStateEnum.INNER_ERROR);
+            return new SeckillResult<SeckillExecution>(false, execution);
+        }
+    }
+
+    //获取系统时间
+    @RequestMapping(value = "/time/now",method = RequestMethod.GET)
+    public SeckillResult<Long> time(){
+        Date now = new Date();
+        return new SeckillResult<Long>(true, now.getTime());
+    }
+
+
+}
